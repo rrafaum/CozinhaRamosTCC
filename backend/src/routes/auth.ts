8806c5344cd64b313443ba5a711f40393b304bd8
Rafael Queiroz Almeida <rafaelqalmeid@gmail.com>
2026-05-05 19:44:06 -0300

    refatorado auth.ts

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { User } from '../models/Usuario';
+import { Usuario } from '../models/Usuario';
 import bcrypt from 'bcryptjs';
 
 const router = Router();
@@ -12,7 +12,7 @@ router.post('/register', async (req, res) => {
     }
     const salt = await bcrypt.genSalt(10);
     const senhaCripto = await bcrypt.hash(senha, salt);
-    const novoUsuario = await User.create({ nome, email, senha: senhaCripto });
+    const novoUsuario = await Usuario.create({ nome, email, senha: senhaCripto });
     res.status(201).json({ id: novoUsuario._id, nome: novoUsuario.nome });
   } catch (err) {
     res.status(400).json({ error: 'Email já cadastrado.' });
@@ -21,9 +21,9 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   const { email, senha } = req.body;
-  const user = await User.findOne({ email });
-  if (user && (await bcrypt.compare(senha, user.senha))) {
-    res.json({ id: user._id, nome: user.nome, token: "fake-jwt-token" });
+  const usuario = await Usuario.findOne({ email });
+  if (usuario && (await bcrypt.compare(senha, usuario.senha))) {
+    res.json({ id: usuario._id, nome: usuario.nome, token: "fake-jwt-token" });
   } else {
     res.status(401).json({ error: 'Credenciais inválidas' });
   }
